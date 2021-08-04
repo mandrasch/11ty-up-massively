744de4f6b5b3ba4aa93a87997b51b28c4bf34751
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2021-08-04 21:55:14 +0200

    minify css after build

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,26 @@
+
+
 module.exports = function (eleventyConfig) {
-    
+
     // copy assets and images folder to _site/ output
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/images");
-    
+
+    // minify css
+    eleventyConfig.on('afterBuild', () => {
+        const CleanCSS = require("clean-css");
+        const fs = require('fs');
+
+        // Run me after the build ends
+        var inputFile = 'src/assets/css/main.css';
+        var input = fs.readFileSync(inputFile, 'utf8');
+        var output = new CleanCSS().minify(input);
+        fs.writeFile('_site/assets/css/main.css', output.styles, function (err) {
+            if (err) return console.log('Error minifying main.css' + err);
+            //success
+          });
+    });
+
     return {
         dir: {
             input: "src",
